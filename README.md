22857dae61475e5b09874cad51459150845704d7
Nathan Dygant <nathan.dygant@gmail.com>
2024-01-03 16:19:22 +0100

    Adding a testing loop of the gym package

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mongomario
 A reinforcement learning approach to playing mario
+
+
+Papers supporting this:
+https://arxiv.org/pdf/1509.06461.pdf
---
+++ test.py
@@ -0,0 +1,24 @@
+"""
+Test of the gym package
+"""
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+from nes_py.wrappers import JoypadSpace
+
+ENV_NAME = "SuperMarioBros-1-1-v0"
+
+env = gym_super_mario_bros.make(
+    ENV_NAME, render_mode="human", apply_api_compatibility=True
+)
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+done = False
+env.reset()
+counter = 0
+while not done:
+    # Choose random action
+    action = env.action_space.sample()
+
+    _, _, done, _, _ = env.step(action)
+    env.render()
